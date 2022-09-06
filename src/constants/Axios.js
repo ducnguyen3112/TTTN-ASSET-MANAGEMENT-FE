08ae09a7fc52129eda619f2fc59192bc4e292aba
Duc Nguyen <ducnguyen201231@gmail.com>
2022-09-07 01:40:46 +0700

    refactor: change axios api url

--- src/constants/Axios.js
+++ src/constants/Axios.js
@@ -1,3 +1,3 @@
-export const AXIOS_API_URL = "http://localhost:8080";
+export const AXIOS_API_URL = "https://am-b5-backend.azurewebsites.net";
 //http://localhost:8080
 //https://am-b5-backend.azurewebsites.net
